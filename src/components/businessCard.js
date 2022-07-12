d1c4ef244fb389fe1b6043722cbcf007fc7bf8c9
Akram Narejo <ak.narejo@gmail.com>
2022-07-12 13:29:58 +0500

    Feat: created signup bar component

--- src/components/businessCard.js
+++ src/components/businessCard.js
@@ -1,19 +1,19 @@
 import {BiRightArrowAlt} from 'react-icons/bi'
 const BusinessCard = (props) => {
-    const {flexWrap, img, imgWidth, contentWidth, title, body, reference, index} = props
+    const {flexWrap, img, title, body, reference, index} = props
     return (
         <div className={`w-full mt-8 flex ${flexWrap} justify-between items-center gap-2`}>
           {
             index/2 === 1 ? (<>
-          <div className={`md:w-[${contentWidth}%] flex flex-col items-start gap-2`}>
-            <h4 className="text-2xl font-bold">{title}</h4>
+          <div className={`w-[100%] md:w-[40%] flex flex-col items-start gap-2`}>
+            <h4 className="text-2xl font-bold text-start">{title}</h4>
             <p className="text-start">{body}</p>
             <p className="flex justify-start items-center text-yellow-300"><a href='.' className="">{reference}</a><BiRightArrowAlt /></p>
           </div>
-          <img src={img} alt={img} className={`md:w-[${imgWidth}%] object-cover h-[300px] rounded-lg shadow-[2px_2px_5px_2px_rgba(0,0,0,0.2)]`}/>
-            </>) : (<><img src={img} alt={img} className={`md:w-[${imgWidth}%] object-cover h-[300px] rounded-lg shadow-[2px_2px_5px_2px_rgba(0,0,0,0.2)]`}/>
-          <div className={`md:w-[${contentWidth}%] flex flex-col items-start gap-2`}>
-            <h4 className="text-2xl font-bold">{title}</h4>
+          <img src={img} alt={img} className={`w-[100%] md:w-[55%] object-cover h-[300px] rounded-lg shadow-[2px_2px_5px_2px_rgba(0,0,0,0.2)]`}/>
+            </>) : (<><img src={img} alt={img} className={`w-[100%] md:w-[55%] object-cover h-[300px] rounded-lg shadow-[2px_2px_5px_2px_rgba(0,0,0,0.2)]`}/>
+          <div className={`w-[100%] md:w-[40%] flex flex-col items-start gap-2`}>
+            <h4 className="text-2xl font-bold text-start">{title}</h4>
             <p className="text-start">{body}</p>
             <p className="flex justify-start items-center text-yellow-300"><a href='.' className="">{reference}</a><BiRightArrowAlt /></p>
           </div></>)
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,16 @@ import background from "../assets/images/mainBackground.avif";
 import promote from '../assets/images/promote.avif'
 import reachCustomers from '../assets/images/reachCustomers.avif'
 import support from '../assets/images/support.avif'
+import delivery from '../assets/images/delivery.avif'
+import phone from '../assets/images/phone.avif'
+import tortilla from '../assets/images/tortilla.svg'
 import blackbanner from "../assets/images/blackbanner.svg";
 import { useState, useEffect } from "react";
 import food from "../assets/icons/food.svg";
 import map from "../assets/icons/map.svg";
 import lightning from "../assets/icons/lightning.svg";
 import BusinessCard from "../components/businessCard";
+import SignupBar from "../components/signupBar";
 
 const STATS = [
   { img: map, count: 148000, caption: "Restaurant partners globally" },
@@ -80,9 +84,7 @@ const Home = () => {
         <div className="w-full mt-8 flex flex-wrap justify-between items-center gap-2">
           <BusinessCard
              flexWrap='flex-wrap'
-             imgWidth='55'
              img={promote}
-             contentWidth='40'
              title='Promote your business'
              body='Boost sales by increasing your visibility on the most popular online channels.'
              reference='Find out more about marketing'
@@ -90,9 +92,7 @@ const Home = () => {
           />
           <BusinessCard
              flexWrap='flex-wrap-reverse'
-             imgWidth='55'
              img={reachCustomers}
-             contentWidth='40'
              title='Reach more customers'
              body='Attract new customers and keep them coming back for more with the delivery type that works for you.'
              reference='Find out more about our delivery services'
@@ -100,9 +100,7 @@ const Home = () => {
           />
           <BusinessCard
              flexWrap='flex-wrap'
-             imgWidth='55'
              img={support}
-             contentWidth='40'
              title='Recieve the support you need'
              body='Watch your business grow with the support of our Restaurant Hub, built to help you succeed.'
              reference='Find out more about Restaurant Hub'
@@ -110,6 +108,90 @@ const Home = () => {
           />
         </div>
       </section>
+
+      <section className="px-2 md:px-8 lg:px-48 text-center mt-8 bg-[#58057d] pt-8">
+        <div className="flex flex-wrap w-full justify-evenly items-center gap-12">
+          <img src={tortilla} alt={tortilla} className='w-[224px]'/>
+          <div className="flex flex-col items-center gap-4 lg:w-[35%]">
+            <h3 className="text-2xl text-white font-bold">"Deliveroo are awesome to work with. They’re super supportive and always suggest new ideas to grow our sales"</h3>
+            <p className="text-white font-bold text-[#C1C7C7]">Head of Marketing, Tortilla</p>
+          </div>
+          <img src={phone} alt='' className='relative top-[-30px] mb-[-30px] lg:top-[-80px] lg:mb-[-80px] w-[336px]'/>
+        </div>
+      </section>
+
+      <section className="px-2 md:px-8 lg:px-48 text-start lg:text-center my-6 pt-8" >
+        <div>
+          <h2 className="text-4xl font-bold">How does Deliveroo work?</h2>
+          <p className="mt-2">Fire up your kitchen for Deliveroo orders in a few easy steps</p>
+        </div>
+        <div className="mt-8 flex justify-between items-center gap-8">
+          <img src={delivery} alt={delivery} className='hidden lg:block w-[50%] h-[340px] object-cover rounded-lg shadow-[2px_2px_5px_2px_rgba(0,0,0,0.2)]'/>
+          <div class="">
+            <div class="flex">
+              <div class="flex flex-col items-center justify-start mr-4">
+                <div>
+                  <div class="flex items-center justify-center w-10 h-10 border border-gray-300 rounded-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-yellow-500" fill="none" viewBox="0 0 24 24"
+                      stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 13l-5 5m0 0l-5-5m5 5V6" />
+                    </svg>
+                  </div>
+                </div>
+                <div class="w-px h-full bg-gray-300"></div>
+              </div>
+              <div class="pb-12">
+                <p class="mb-2 text-xl font-bold text-start">Sign Up</p>
+                <p class="text-gray-700 text-start">
+                Tell us about your business - you'll need your FSA rating to get started
+                </p>
+              </div>
+            </div>
+
+            <div class="flex">
+              <div class="flex flex-col items-center justify-start mr-4">
+                <div>
+                  <div class="flex items-center justify-center w-10 h-10 border border-gray-300 rounded-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-yellow-500" fill="none" viewBox="0 0 24 24"
+                      stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 13l-5 5m0 0l-5-5m5 5V6" />
+                    </svg>
+                  </div>
+                </div>
+                <div class="w-px h-full bg-gray-300"></div>
+              </div>
+              <div class="pb-12">
+                <p class="mb-2 text-xl font-bold text-start">Set Up</p>
+                <p class="text-gray-700 text-start">
+                Upload a menu and share your store hours
+                </p>
+              </div>
+            </div>
+
+            <div class="flex">
+              <div class="flex flex-col items-center mr-4">
+                <div>
+                  <div class="flex items-center justify-center w-10 h-10 border border-gray-300 rounded-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-yellow-500" fill="none" viewBox="0 0 24 24"
+                      stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              <div class="pt-1">
+                <p class="mb-2 text-xl font-bold text-start">Sell</p>
+                <p className="text-gray-700 text-start">Watch the orders come in and track them via <a href='.' className="text-yellow-400 cursor-pointer">Restaurant Hub</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      
+      <div className="my-8">
+        <SignupBar title='Not a Deliveroo parneter yet?' btnText="Create account"/>
+      </div>
+      <p>hello world</p>
     </>
   );
 };
